50193ddd5aedfbb68cf3672af15f7a014e1acded
parth-lth <parthpkul@gmail.com>
2024-06-18 01:48:07 +0530

    Add assignee

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, nullable=False)
     description = db.Column(db.String(120), index=True, nullable=False)
+    assigned_to = db.Column(db.String(64), index=True, nullable=True)
     status = db.Column(db.Boolean, default=False)
